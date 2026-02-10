6a1ef49d9241fa3e373185acaf6eaf32e70027d8
mioivanov444 <glagol321@abv.bg>
2026-02-10 21:13:09 +0200

    Finished gun detail page with ratings and reviews

--- guns/views.py
+++ guns/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Gun
 from reviews.models import Review
 from reviews.forms import ReviewForm
+from django.db.models import Avg
+import math
 
 def gun_list(request):
     guns = Gun.objects.all()
@@ -10,6 +12,10 @@ def gun_list(request):
 def gun_detail(request, slug):
     gun = get_object_or_404(Gun, slug=slug)
     reviews = gun.reviews.all().order_by('-created_at')
+
+    #Average rating
+    average_rating = reviews.aggregate(Avg('rating'))['rating__avg'] or 0
+    average_rating_int = math.floor(average_rating)
 
     if request.method == 'POST':
         form = ReviewForm(request.POST)
@@ -21,9 +27,13 @@ def gun_detail(request, slug):
     else:
         form = ReviewForm()
 
+
+
     context = {
         'gun': gun,
         'reviews': reviews,
         'form': form,
+        'average_rating': average_rating,
+        'average_rating_int': average_rating_int,
     }
     return render(request, 'guns/gun_detail.html', context)
